caffe3a05c52f03d57a24901ea552ad64791f277
Cristian Perez <cperezlecaros@gmail.com>
2025-07-13 22:22:17 -0400

    Add profile endpoint

--- src/main/java/foro/hub/api/controllers/AuthController.java
+++ src/main/java/foro/hub/api/controllers/AuthController.java
@@ -1,15 +1,19 @@
 package foro.hub.api.controllers;
 
+import foro.hub.api.dto.DatosDetalleUsuario;
 import foro.hub.api.dto.JwtResponse;
 import foro.hub.api.dto.LoginRequest;
+import foro.hub.api.repositories.UsuarioRepository;
 import foro.hub.api.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +23,7 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UsuarioRepository usuarioRepository;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest loginData){
@@ -35,6 +40,20 @@ public class AuthController {
 
         return ResponseEntity.ok(new JwtResponse(token));
 
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getProfile(){
+        //esto obtiene el objeto authentication que se crea en el filter jwt
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+        //getPrincipal retorna un Object
+        var userEmail = (String) auth.getPrincipal();
+        var user = usuarioRepository.findByEmail(userEmail).orElse(null);
+
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(new DatosDetalleUsuario(user));
     }
 
     //este metodo retornara unauthorized al enviar credenciales erroneas
